952331b691ebd8e0c6c27020342a9572f9fb703a
Yulia Kostrikova <yuklia.dev@gmail.com>
2020-11-22 20:17:01 +0200

    👾 Fix typos in AZ. Add readme

--- README.md
+++ README.md
@@ -1,35 +1,5 @@
-Usage:
+# Custom PaaS on AWS
 
-export AWS_SHARED_CREDENTIALS_FILE=""
-export AWS_PROFILE=""
-terraform13 plan
-terraform13 state list
-terraform13 state show module.vpc-usa-oregon.aws_vpc.vpc
+## Networking Architecture Example
 
-destroy preview
-terraform13 plan -destroy
-
-------
-Modules can either be loaded from the local filesystem, or a remote source
-
-
-flexible, reusable, and composable.
-
-
-https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-
-regexp : re2 type
-https://github.com/google/re2/wiki/Syntax
-
-Tests
-https://terratest.gruntwork.io/
-
-Default envs 
-https://www.terraform.io/docs/commands/environment-variables.html
-
-
-
-------
-TODO next:
-- Vault https://blog.gruntwork.io/a-comprehensive-guide-to-managing-secrets-in-your-terraform-code-1d586955ace1
-
+![Physical deployment schema](doc/imgs/paas-swarm-aws-network.png)
--- core/variables.tf
+++ core/variables.tf
@@ -29,7 +29,7 @@ variable "vpc_paris" {
     private_subnets = {
         count = 3,
         cidr_block = ["20.10.4.0/24", "20.10.5.0/24", "20.10.6.0/24"],
-        az = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+        az = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
     }
   }
 }
@@ -69,48 +69,3 @@ variable "vpc_oregon" {
     }
   }
 }
-
-# variable "vpc_name" {
-#   description = "Name of VPC"
-#   type        = string
-#   default     = "example-vpc"
-# }
-
-# variable "vpc_cidr" {
-#   description = "CIDR block for VPC"
-#   type        = string
-#   default     = "10.0.0.0/16"
-# }
-
-# variable "vpc_azs" {
-#   description = "Availability zones for VPC"
-#   type        = list
-#   default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
-# }
-
-# variable "vpc_private_subnets" {
-#   description = "Private subnets for VPC"
-#   type        = list(string)
-#   default     = ["10.0.1.0/24", "10.0.2.0/24"]
-# }
-
-# variable "vpc_public_subnets" {
-#   description = "Public subnets for VPC"
-#   type        = list(string)
-#   default     = ["10.0.101.0/24", "10.0.102.0/24"]
-# }
-
-# variable "vpc_enable_nat_gateway" {
-#   description = "Enable NAT gateway for VPC"
-#   type    = bool
-#   default = true
-# }
-
-# variable "vpc_tags" {
-#   description = "Tags to apply to resources created by VPC module"
-#   type        = map(string)
-#   default     = {
-#     Terraform   = "true"
-#     Environment = "dev"
-#   }
-# }
